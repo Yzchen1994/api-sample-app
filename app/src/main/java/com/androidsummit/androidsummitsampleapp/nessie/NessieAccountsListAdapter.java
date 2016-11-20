dac22405df5b7fbd144e4dfec0972725f8249c5e
Yuze <yuze.chen@duethealth.com>
2016-11-19 23:16:17 -0500

    bind account adapter

--- app/src/main/java/com/androidsummit/androidsummitsampleapp/nessie/NessieAccountsListAdapter.java
+++ app/src/main/java/com/androidsummit/androidsummitsampleapp/nessie/NessieAccountsListAdapter.java
@@ -6,7 +6,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
+import com.androidsummit.androidsummitsampleapp.R;
 import com.reimaginebanking.api.nessieandroidsdk.models.Account;
 
 import java.util.List;
@@ -29,6 +31,13 @@ public class NessieAccountsListAdapter extends ArrayAdapter<Account> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         View view = convertView;
+        TextView tvCurrentBalance, tvRewardBalance, tvAccountName, tvEndingIn;
+
+        tvCurrentBalance = (TextView) view.findViewById(R.id.account_adapter_current_balance_tv);
+        tvRewardBalance = (TextView) view.findViewById(R.id.account_adapter_reward_balance_tv);
+        tvAccountName = (TextView) view.findViewById(R.id.account_adapter_account_name_tv);
+        tvEndingIn = (TextView) view.findViewById(R.id.account_adapter_ending_tv);
+
         if (view == null) {
             LayoutInflater inflator = LayoutInflater.from(getContext());
             view = inflator.inflate(mResourceId, null);
@@ -37,9 +46,25 @@ public class NessieAccountsListAdapter extends ArrayAdapter<Account> {
         Account account = mAccounts.get(position);
 
         if (account != null) {
-            //TODO: Bind xml with members in account
+            // Bind xml with members in account
+            tvCurrentBalance.setText(account.getBalance());
+            tvRewardBalance.setText(account.getRewards());
+            tvAccountName.setText(account.getNickname());
+            tvEndingIn.setText(getEndInStr(account.getAccountNumber()));
         }
 
         return view;
+    }
+
+    private String getEndInStr(String str) {
+        try {
+            String result = "(Ending in ";
+            result += str.substring(str.length() - 1, str.length() - 4);
+            result += ")";
+            return result;
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
